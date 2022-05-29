feac4fa88c05cc425a6112cd91e8474a19558b36
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2022-05-29 13:24:41 +0500

    Init

---
+++ action.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.common.by import By
+
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+driver.get("https://vulms.vu.edu.pk/LMS_LP.aspx")
+
+captcha_text = driver.find_element(By.ID, "g-recaptcha-response")
+print(captcha_text.get_attribute('value'))
