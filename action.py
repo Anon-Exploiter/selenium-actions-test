bc53c79c99a13b16e16015a2447b26cbcd18b7eb
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2022-05-29 13:26:26 +0500

    Update action.py

--- action.py
+++ action.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 from selenium.webdriver.common.by import By
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+chrome_service = Service(ChromeDriverManager().install())
 
 chrome_options = Options()
 options = [
